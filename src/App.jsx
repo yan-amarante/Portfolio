735dad35122f0c5214e73e17536685b15091bef5
YAN LIMA AMARANTE <yanlimaamarantec@hotmail.com>
2023-10-23 21:32:22 -0300

    feat: add home and header component and reset for css

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,26 @@ function App() {
 
   return (
     <section className='container'>
-      
+      <Header/>
+      <Home/>
     </section>
+  )
+}
+
+function Home () {
+  return(
+    <main className='container-home'>
+      <h2>Home</h2>
+    </main>
+  )
+}
+
+
+function Header () {
+  return(
+    <header className='container-header'>
+      <h2>Yan Amarante</h2>
+    </header>
   )
 }
 
