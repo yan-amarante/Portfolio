8a4f1857f2f9d345665e1e1e8f0fa7c3154768bf
YAN LIMA AMARANTE <yanlimaamarantec@hotmail.com>
2023-10-23 21:53:30 -0300

    feat: add article and a animation to the home

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
 import './App.css'
+import { Player } from "@lottiefiles/react-lottie-player";
 
 function App() {
 
   return (
     <section className='container'>
-      <Header/>
-      <Home/>
+      <Header />
+      <Home />
     </section>
   )
 }
 
-function Home () {
-  return(
+function Home() {
+  return (
     <main className='container-home'>
-      <h2>Home</h2>
+      <article className='artigo-home'>
+        <h1 className='titulo-home'>Teste Titulo</h1>
+        <p className='descricao-home'>Teste descrição descrição</p>
+      </article>
+      <Player
+        src="https://assets-v2.lottiefiles.com/a/5017fc96-1177-11ee-b93e-0b578c668bf6/f95gEUFQZt.json"
+        loop
+        autoplay
+        speed={0.5}
+        style={{ height: "50%", width: "50%" }}
+      />
     </main>
   )
 }
 
 
-function Header () {
-  return(
+function Header() {
+  return (
     <header className='container-header'>
-      <h2>Yan Amarante</h2>
+      <h2 className='titulo-header'>Yan Amarante</h2>
     </header>
   )
 }
